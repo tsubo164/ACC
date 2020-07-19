4211ebbc8cbacaa0cfff7afbf59ec530f75655e5
Hiroshi Tsubokawa <hiroshi@imac.local>
2020-07-19 12:05:44 -0700

    reading a number from file

--- Makefile
+++ Makefile
@@ -3,5 +3,9 @@
 mcc: main.c
 	gcc -O2 -Wall -std=c89 --pedantic-errors -o $@ $<
 
+run:
+	./mcc input.mc
+	gcc input.s
+
 clean:
-	rm -f mcc a.out a.s
+	rm -f mcc a.out *.s
--- main.c
+++ main.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
+#include <ctype.h>
 
-int main()
+int main(int argc, char **argv)
 {
-  FILE *file;
+  FILE *file = NULL;
+  int c = '\0';
+  int n = 0;
 
-  file = fopen("a.s", "w");
+  if (argc != 2) {
+    printf("mcc: error: no input files\n");
+    return -1;
+  }
+
+  file = fopen(argv[1], "r");
   if (!file) {
     return -1;
   }
 
+  c = fgetc(file);
+  if (!isdigit(c)) {
+    printf("error: the character is not a digit: %c\n", c);
+    fclose(file);
+    return -1;
+  }
+  fclose(file);
+
+  file = fopen("input.s", "w");
+  if (!file) {
+    return -1;
+  }
+
+  n = c - '0';
+
   fprintf(file, ".intel_syntax noprefix\n");
   fprintf(file, ".global _main\n");
   fprintf(file, "_main:\n");
-  fprintf(file, "  mov rax, 42\n");
+  fprintf(file, "  mov rax, %d\n", n);
   fprintf(file, "  ret\n");
   fclose(file);
 
