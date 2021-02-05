a237b24d5f98eff5be9c95d0cd8f3c8954f9caa9
Hiroshi Tsubokawa <hiroshi@fujiyama-renderer.com>
2021-02-05 15:27:45 -0800

    added access to values in multi-dimension array

--- gen_x86.c
+++ gen_x86.c
@@ -432,6 +432,32 @@ static void code3__(FILE *fp, const struct ast_node *node,
 /* forward declaration */
 static void gen_code(FILE *fp, const struct ast_node *node);
 
+static void gen_addr(FILE *fp, const struct ast_node *node)
+{
+    if (!node)
+        return;
+
+    switch (node->kind) {
+
+    case NOD_DEREF:
+        gen_code(fp, node->l);
+        break;
+
+    default:
+        break;
+    }
+}
+
+static void load_value(FILE *fp, const struct ast_node *node)
+{
+    const struct data_type *type = node->type;
+
+    if (is_array(type))
+        return;
+
+    code3__(fp, node, MOV_, addr1(RAX), A_);
+}
+
 static int gen_one_param(FILE *fp, const struct ast_node *node, int reg_index)
 {
     int max_index = 0;
@@ -1005,7 +1031,12 @@ static void gen_code(FILE *fp, const struct ast_node *node)
 
     case NOD_DEREF:
         gen_code(fp, node->l);
+        if (!is_array(node->type))
+            code3__(fp, node, MOV_, addr1(RAX), A_);
+#if 0
+        gen_code(fp, node->l);
         code3__(fp, node, MOV_, addr1(RAX), A_);
+#endif
         break;
 
     case NOD_NUM:
--- input.c
+++ input.c
@@ -2,7 +2,7 @@ int main()
 {
     int a[3][2];
 
-    //a[2][1] = 13;
+    a[2][1] = 13;
 
-    return sizeof a;//a[2][1];
+    return a[2][1];
 }
