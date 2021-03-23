8482c05a73aa0db7d91a52dd3f51351caf394836
Hiroshi Tsubokawa <hiroshi@fujiyama-renderer.com>
2021-03-23 09:52:28 -0700

    Improve argument count check

--- input.c
+++ input.c
@@ -5,6 +5,7 @@ int inc(int *i)
 {
     return ++*i;
 }
+*/
 int foo(void)
 {
     return 42;
@@ -14,22 +15,21 @@ int add(int x, int y, int z)
 {
     return x + y + z;
 }
-*/
 
 int main()
 {
+    int i = add(11, 14, 29);
     /*
-    int i = -27 % 7;
-    printf("i: %d\n", i);
-    int i = add();
     int i = foo();
     int i = foo(11);
-    int i = add(11, 14);
+    int i = -27 % 7;
+    printf("i: %d\n", i);
+    int i = foo();
+    int i = add();
     int i = add(11, 14);
     int i = 13;
 
     i = inc(&i);
-    */
         long a;// = 233334012321;
         //unsigned short b;// = 2334;
         long b;// = 2334;
@@ -46,6 +46,7 @@ int main()
         //c = a - b;
 
         //printf("c = %d\n", c);
+    */
 
-    return c;
+    return i;
 }
